58a36661ea47f97b7a01fe1a6953c6d5542ea23a
Ferose Khan J <ferosekhanj@yahoo.com>
2013-10-19 16:50:42 +0530

    fixed a bug in authentication and layout

--- ProjectOne/Controllers/HomeController.cs
+++ ProjectOne/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebMatrix.WebData;
 
 namespace ProjectOne.Controllers
 {
@@ -14,6 +15,9 @@ namespace ProjectOne.Controllers
 
         public ActionResult Index()
         {
+            if (WebSecurity.IsAuthenticated && WebSecurity.HasUserId)
+                return RedirectToAction("Index", "Log");
+
             return View(new SignupViewModel { TimezoneOffset = 0.0 });
         }
 
